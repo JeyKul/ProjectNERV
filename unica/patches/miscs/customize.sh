a291a541659a4e2ea316efe89b09ddd8b26802c8
JeyKul <77121325+JeyKul@users.noreply.github.com>
2025-07-27 01:09:35 +0200

    unica/patches/miscs: Rebrand ROM

--- unica/patches/miscs/customize.sh
+++ unica/patches/miscs/customize.sh
@@ -44,7 +44,7 @@ TARGET_DEVICE="$TARGET_CODENAME"
 [ -n "$TARGET_MODEL" ] && TARGET_DEVICE="$TARGET_MODEL"
 
 # Set custom Display ID prop
-SET_PROP "system" "ro.build.display.id" "Project NERV $(echo -n ${ROM_VERSION} | cut -d "-" -f1)-${ROM_CODENAME} - ${TARGET_DEVICE} [$(GET_PROP "system" "ro.build.display.id")]"
+SET_PROP "system" "ro.build.display.id" "Terracotta Rom | Based on UN1CA and Project NERV | $(echo -n ${ROM_VERSION} | cut -d "-" -f1)-${ROM_CODENAME} - ${TARGET_DEVICE} [$(GET_PROP "system" "ro.build.display.id")]"
 
 # Crok's RAM Managment Fix
 # https://github.com/crok/crokrammgmtfix/blob/master/service.sh#L27-L32
