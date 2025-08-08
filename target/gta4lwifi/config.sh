064158c5b7e3b2b3041786ddb46b17ead9f69bf6
Enda <endadwagon@gmail.com>
2025-08-09 00:57:05 +0100

    target: gta4lwifi: init

---
+++ target/gta4lwifi/config.sh
@@ -0,0 +1,57 @@
+#
+# Copyright (C) 2024 Salvo Giangreco
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Device configuration file for Galaxy S21 FE 5G (Snapdragon) (r9q)
+TARGET_NAME="Galaxy Tab A7 (wifi)"
+TARGET_CODENAME="gta4lwifi"
+TARGET_ASSERT_MODEL=("SM-T500")
+TARGET_FIRMWARE="SM-T500/EUX/R9WN81PNL4J"
+TARGET_EXTRA_FIRMWARES=()
+TARGET_API_LEVEL=31
+TARGET_PRODUCT_FIRST_API_LEVEL=29
+TARGET_VENDOR_API_LEVEL=30
+TARGET_SINGLE_SYSTEM_IMAGE="gts9pwifi"
+TARGET_OS_FILE_SYSTEM="erofs"
+TARGET_FS_CHANGED="true"
+TARGET_SUPER_PARTITION_SIZE=5729419264
+TARGET_SUPER_GROUP_SIZE=5725224960
+TARGET_HAS_SYSTEM_EXT=false
+
+# SEC Product Feature
+TARGET_AUDIO_SUPPORT_ACH_RINGTONE=false
+TARGET_AUDIO_SUPPORT_DUAL_SPEAKER=true
+TARGET_AUDIO_SUPPORT_VIRTUAL_VIBRATION=false
+TARGET_AUTO_BRIGHTNESS_TYPE="3"
+TARGET_DVFS_CONFIG_NAME="dvfs_policy_sdm8259_xx"
+TARGET_ESE_CHIP_VENDOR="MULTI"
+TARGET_ESE_COS_NAME="U5.1_J5.3T"
+TARGET_FP_SENSOR_CONFIG="google_touch_display_side"
+TARGET_HAS_HW_MDNIE=true
+TARGET_HAS_MASS_CAMERA_APP=false
+TARGET_HAS_QHD_DISPLAY=false
+#TARGET_HFR_MODE="1"
+TARGET_HFR_MODE="2"
+TARGET_HFR_SUPPORTED_REFRESH_RATE="60"
+TARGET_HFR_DEFAULT_REFRESH_RATE="60"
+TARGET_IS_ESIM_SUPPORTED=false
+TARGET_MDNIE_SUPPORTED_MODES="45073"
+TARGET_MDNIE_WEAKNESS_SOLUTION_FUNCTION="0"
+TARGET_MULTI_MIC_MANAGER_VERSION="07020"
+TARGET_SSRM_CONFIG_NAME="siop_gts7xl_sm8250"
+TARGET_SUPPORT_CUTOUT_PROTECTION=false
+TARGET_DISABLE_AVB_SIGNING=true
+IS_TABLET=true
