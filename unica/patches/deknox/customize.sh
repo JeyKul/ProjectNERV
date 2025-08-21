b1b48b38858ba79ffef466c2bb75a8eee95a57f2
JeyKul <jey@kulovac.de>
2025-08-21 20:34:19 +0200

    deknox: Force the qssi patch

--- unica/patches/deknox/customize.sh
+++ unica/patches/deknox/customize.sh
@@ -1,17 +1,6 @@
 SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_FRAMEWORK_SUPPORT_BLOCKCHAIN_SERVICE" --delete
 
-if [ "$TARGET_SINGLE_SYSTEM_IMAGE" == "qssi" ]; then
     ADD_TO_WORK_DIR "a05snsdxx" "system" "."
-elif [ "$TARGET_SINGLE_SYSTEM_IMAGE" == "essi" ]; then
-    ADD_TO_WORK_DIR "gts9fexx" "system" "system/bin" 0 2000 751 "u:object_r:system_file:s0"
-    ADD_TO_WORK_DIR "gts9fexx" "system" "system/lib/libandroid_servers.so" 0 0 644 "u:object_r:system_lib_file:s0"More actions
-    ADD_TO_WORK_DIR "gts9fexx" "system" "system/lib/libmdf.so" 0 0 644 "u:object_r:system_lib_file:s0"
-    ADD_TO_WORK_DIR "gts9fexx" "system" "system/lib64/libandroid_servers.so" 0 0 644 "u:object_r:system_lib_file:s0"
-    ADD_TO_WORK_DIR "gts9fexx" "system" "system/lib64/libepm.so" 0 0 644 "u:object_r:system_lib_file:s0"
-    ADD_TO_WORK_DIR "gts9fexx" "system" "system/lib64/libmdf.so" 0 0 644 "u:object_r:system_lib_file:s0"
-elif [ "$TARGET_SINGLE_SYSTEM_IMAGE" == "self" ]; then
-    return 0
-fi
 
 DELETE_FROM_WORK_DIR "system" "system/app/BlockchainBasicKit"
 DELETE_FROM_WORK_DIR "system" "system/bin/dualdard"
