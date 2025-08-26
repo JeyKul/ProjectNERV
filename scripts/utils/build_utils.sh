451802002be5671dbfca7f5f9425a3da32f7ee1f
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-08-26 12:51:41 +0200

    scripts: utils: build_utils: Add 7z
    Change-Id: I47f99ac44d3e1777fc57f6ed5528be82eca7d07c
    Signed-off-by: Ksawlii <maja.blaszczak@proton.me>

--- scripts/utils/build_utils.sh
+++ scripts/utils/build_utils.sh
@@ -112,7 +112,7 @@ DEPENDENCIES=(
     "lz4" "make" "md5sum" "mkdir" "mount" "mv" "pcre2test"
     "perl" "protoc" "python3" "rm" "sed" "sha1sum" "sort"
     "stat" "tail" "tar" "touch" "tr" "truncate" "umount"
-    "unzip" "wc" "whoami" "xargs" "xxd" "zip" "zstd"
+    "unzip" "wc" "whoami" "xargs" "xxd" "zip" "zstd" "7z"
 )
 MISSING=()
 for d in "${DEPENDENCIES[@]}"; do
