5685822b3d4b651a5899cc35671feb5cfd059440
JeyKul <jey@kulovac.de>
2025-08-29 20:59:47 +0200

    gts7lwifi: Add Encryption

--- target/gts7lwifi/patches/filesystem/customize.sh
+++ target/gts7lwifi/patches/filesystem/customize.sh
@@ -11,11 +11,11 @@ sed -i '/^\(product\|vendor\|odm\)[[:space:]]\+\/\(product\|vendor\|odm\)[[:spac
   s/ext4/erofs/
 }' $WORK_DIR/vendor/etc/fstab.qcom
 
-sed -i 's/fileencryption=ice//g' $WORK_DIR/vendor/etc/fstab.qcom
+sed -i 's/fileencryption=ice/fileencryption=aes-256-xts:aes-256-cts:v2+inlinecrypt_optimized/g' $WORK_DIR/vendor/etc/fstab.qcom
 
 echo "Remove DualDAR mount points"
 sed -i "/keydata/d" "$WORK_DIR/vendor/etc/fstab.qcom"
 sed -i "/keyrefuge/d" "$WORK_DIR/vendor/etc/fstab.qcom"
 
 echo "Replace USERDATA string"
-sed -i 's|^/dev/block/bootdevice/by-name/userdata[[:space:]]\+/data[[:space:]]\+f2fs[[:space:]]\+noatime,nosuid,nodev,discard,usrquota,grpquota,fsync_mode=nobarrier,reserve_root=32768,resgid=5678,inlinecrypt[[:space:]]\+latemount,wait,check,fileencryption=ice,quota,reservedsize=128M,checkpoint=fs$|/dev/block/bootdevice/by-name/userdata  /data  f2fs  noatime,nosuid,nodev,discard,usrquota,grpquota,fsync_mode=nobarrier,reserve_root=32768,resgid=5678,inlinecrypt  latemount,wait,check,quota,reservedsize=128M,sysfs_path=/sys/devices/platform/soc/1d84000.ufshc,checkpoint=fs,keydirectory=/metadata/vold/metadata_encryption|' $WORK_DIR/vendor/etc/fstab.qcom
+sed -i 's|^/dev/block/bootdevice/by-name/userdata[[:space:]]\+/data[[:space:]]\+f2fs[[:space:]]\+noatime,nosuid,nodev,discard,usrquota,grpquota,fsync_mode=nobarrier,reserve_root=32768,resgid=5678,inlinecrypt[[:space:]]\+latemount,wait,check,fileencryption=ice,quota,reservedsize=128M,checkpoint=fs$|/dev/block/bootdevice/by-name/userdata  /data  f2fs  noatime,nosuid,nodev,discard,usrquota,grpquota,fsync_mode=nobarrier,reserve_root=32768,resgid=5678,inlinecrypt  latemount,wait,check,,fileencryption=aes-256-xts:aes-256-cts:v2+inlinecrypt_optimized,quota,reservedsize=128M,sysfs_path=/sys/devices/platform/soc/1d84000.ufshc,checkpoint=fs,keydirectory=/metadata/vold/metadata_encryption|' $WORK_DIR/vendor/etc/fstab.qcom
