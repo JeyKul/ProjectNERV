44bcd70ade6e7b10bde8042da99ef04a7ec83fe5
Sandeep Prasad <sandeep.prasad30@outlook.com>
2025-08-31 13:42:17 -0700

    gts7fewifi: Welcome

---
+++ target/gts7fewifi/config.sh
@@ -0,0 +1,59 @@
+#
+# Copyright (C) 2024 BlackMesa123
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Device configuration file for Galaxy S20 FE (Qualcomm) (r8q)
+TARGET_NAME="Galaxy Tab S7 FE"
+TARGET_CODENAME="gts7fewifi"
+TARGET_ASSERT_MODEL=("SM-T733")
+TARGET_FIRMWARE="SM-T733/EUY/R52T700GT1Y"
+TARGET_EXTRA_FIRMWARES=()
+TARGET_API_LEVEL=33
+TARGET_PRODUCT_FIRST_API_LEVEL=30
+TARGET_VENDOR_API_LEVEL=30
+TARGET_SINGLE_SYSTEM_IMAGE="gts9pwifi"
+TARGET_OS_FILE_SYSTEM="erofs"
+TARGET_FS_CHANGED=true
+TARGET_SUPER_PARTITION_SIZE=8325693440
+TARGET_SUPER_GROUP_SIZE=8321499136
+TARGET_HAS_SYSTEM_EXT=false
+#TARGET_PLATFORM="sm7325"
+
+# SEC Product Feature
+TARGET_AUDIO_SUPPORT_ACH_RINGTONE=false
+TARGET_AUDIO_SUPPORT_DUAL_SPEAKER=true
+TARGET_AUDIO_SUPPORT_VIRTUAL_VIBRATION=false
+TARGET_AUTO_BRIGHTNESS_TYPE="3"
+TARGET_DVFS_CONFIG_NAME="dvfs_policy_sm7325_xx"
+TARGET_ESE_CHIP_VENDOR="MULTI"
+TARGET_ESE_COS_NAME="U5.1_J5.3T"
+TARGET_FP_SENSOR_CONFIG=false
+TARGET_HAS_HW_MDNIE=false
+TARGET_HAS_MASS_CAMERA_APP=false
+TARGET_HAS_QHD_DISPLAY=false
+#TARGET_HFR_MODE="1"
+TARGET_HFR_MODE="0"
+TARGET_HFR_SUPPORTED_REFRESH_RATE="60"
+TARGET_HFR_DEFAULT_REFRESH_RATE="60"
+TARGET_HFR_SEAMLESS_BRT="none"
+TARGET_HFR_SEAMLESS_LUX="none"
+TARGET_IS_ESIM_SUPPORTED=false
+TARGET_MDNIE_SUPPORTED_MODES="0"
+TARGET_MDNIE_WEAKNESS_SOLUTION_FUNCTION="0"
+TARGET_MULTI_MIC_MANAGER_VERSION="07020"
+TARGET_SSRM_CONFIG_NAME="siop_gts7fewifi_sm7325"
+TARGET_SUPPORT_CUTOUT_PROTECTION=false
+TARGET_DISABLE_AVB_SIGNING=true
