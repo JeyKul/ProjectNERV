e1cb2dc73f401a3a27a30a4f431187fef5cf5c0c
Saad <saadelasfur@gmail.com>
2025-09-12 13:59:57 +0300

    unica: patches: deknox: burp
    Not required anyway
    
    Signed-off-by: Saad <saadelasfur@gmail.com>

--- unica/patches/deknox/tssi/customize.sh
+++ unica/patches/deknox/tssi/customize.sh
@@ -1,7 +1,3 @@
-if [[ "$TARGET_SINGLE_SYSTEM_IMAGE" == "self" ]]; then
-    return 0
-fi
-
 SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_FRAMEWORK_SUPPORT_BLOCKCHAIN_SERVICE" --delete
 
 ADD_TO_WORK_DIR "a05snsdxx" "system" "."
